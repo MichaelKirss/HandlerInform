40f3fd3b2cfb9bb6bceab987ff20db3d257a4a65
Michael Kirss <Michaelkirss@gmail.com>
2023-10-11 19:21:12 +0400

    Delete target and idea catalog

--- src/main/java/org/skillfactory/main/Main.java
+++ src/main/java/org/skillfactory/main/Main.java
@@ -1,5 +1,9 @@
 package org.skillfactory.main;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.skillfactory.comparators.StudentComparator;
 import org.skillfactory.comparators.UniversityComparator;
 import org.skillfactory.comparators.choice.ChoiceTypeComparator;
@@ -7,12 +11,14 @@ import org.skillfactory.comparators.enums.StudentComparators;
 import org.skillfactory.comparators.enums.UniversityComparators;
 import org.skillfactory.model.Student;
 import org.skillfactory.model.University;
+import org.skillfactory.utilites.JsonUtil;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-import static org.skillfactory.load.ExcelLoader.*;
+import static org.skillfactory.load.ExcelLoader.parserStudent;
+import static org.skillfactory.load.ExcelLoader.parserUniversity;
+import static org.skillfactory.utilites.JsonUtil.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -23,10 +29,15 @@ public class Main {
         ChoiceTypeComparator.UniversitysComparatorsChoice(UniversityComparators.YEAR);
         StudentComparator studentComparator =
         ChoiceTypeComparator.StudentsComparatorsChoice(StudentComparators.AVG_EXAM_SCORE);
-        System.out.println("Университеты");
-        u.stream().sorted(universityComparator).forEach(System.out::println);
-        System.out.println("Студенты");
-        s.stream().sorted(studentComparator).forEach(System.out::println);
+//        System.out.println("Университеты");
+//        u.stream().sorted(universityComparator).forEach(System.out::println);
+//        System.out.println("Студенты");
+//        s.stream().sorted(studentComparator).forEach(System.out::println);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String sJs = gson.toJson(s);
+        System.out.println(sJs);
+
     }
 
 }
---
+++ src/main/java/org/skillfactory/utilites/JsonUtil.java
@@ -0,0 +1,14 @@
+package org.skillfactory.utilites;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.skillfactory.model.Student;
+
+public class JsonUtil {
+    private JsonUtil(){}
+        public static void  serialJs (Student student){
+
+        }
+}
